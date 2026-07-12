51ab42bf90d501471f47beb6cf36a22c910e54d6
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-12 19:14:20 +0530

    Add variables example in Python

---
+++ 01_variables/variables.py
@@ -0,0 +1,11 @@
+# Variables in Python
+
+name = "Shreya"
+age = 22
+height = 5.7
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
