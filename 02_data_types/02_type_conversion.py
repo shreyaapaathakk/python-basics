648f9f3ff874ddd06daa1f10442b796d88e7648f
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-18 08:45:25 +0530

    Create 02_type_conversion.py

---
+++ 02_data_types/02_type_conversion.py
@@ -0,0 +1,27 @@
+"""
+Type Conversion in Python
+
+This script demonstrates how to convert one data type into another.
+"""
+
+# String to Integer
+age = "22"
+print("String to Integer:", int(age))
+
+# Integer to Float
+marks = 95
+print("Integer to Float:", float(marks))
+
+# Float to Integer
+price = 199.99
+print("Float to Integer:", int(price))
+
+# Integer to String
+roll_number = 101
+print("Integer to String:", str(roll_number))
+
+# Boolean Conversion
+print("Boolean of 1:", bool(1))
+print("Boolean of 0:", bool(0))
+print("Boolean of 'Python':", bool("Python"))
+print("Boolean of '':", bool(""))
