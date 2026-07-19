972d525f2d382942a5e1d7349953453554261476
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-19 09:03:11 +0530

    Add identity operators demonstration script
    This script demonstrates the use of identity operators in Python.

---
+++ 03_operators/05_identity.py
@@ -0,0 +1,14 @@
+"""
+Identity Operators
+
+This script demonstrates identity operators.
+"""
+
+list1 = [1, 2, 3]
+list2 = list1
+list3 = [1, 2, 3]
+
+print("list1 is list2:", list1 is list2)
+print("list1 is list3:", list1 is list3)
+
+print("list1 == list3:", list1 == list3)
