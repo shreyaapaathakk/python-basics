4ac6b9b6f1900248984a303091ab6d64f911d79b
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-19 09:09:48 +0530

    Add f-strings demonstration script
    This script demonstrates how to use f-strings for string formatting in Python.

---
+++ 04_input_output/04_f_strings.py
@@ -0,0 +1,13 @@
+"""
+Formatted Strings (f-strings)
+
+This script demonstrates string formatting using f-strings.
+"""
+
+name = "Shreya"
+language = "Python"
+experience = 1
+
+print(f"My name is {name}.")
+print(f"I am learning {language}.")
+print(f"I have {experience} year of programming experience.")
