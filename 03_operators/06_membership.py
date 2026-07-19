5ab95986dd64eb83ef231eb81bbe25f8112405d7
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-19 09:03:41 +0530

    Add membership operators demonstration script
    This script demonstrates the use of membership operators with a list of fruits.

---
+++ 03_operators/06_membership.py
@@ -0,0 +1,12 @@
+"""
+Membership Operators
+
+This script demonstrates membership operators.
+"""
+
+fruits = ["Apple", "Banana", "Mango"]
+
+print("'Apple' in fruits:", "Apple" in fruits)
+print("'Orange' in fruits:", "Orange" in fruits)
+
+print("'Banana' not in fruits:", "Banana" not in fruits)
