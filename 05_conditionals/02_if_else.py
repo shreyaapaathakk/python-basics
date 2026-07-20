849024db9fe04964c264a1c3b6d2dcecd154d7ab
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-20 13:51:39 +0530

    Add if...else statement demonstration script
    This script demonstrates the use of if...else statements to check if a number is even or odd and to determine driving license eligibility based on age.

---
+++ 05_conditionals/02_if_else.py
@@ -0,0 +1,20 @@
+"""
+The if...else Statement
+
+This script demonstrates if...else.
+"""
+
+number = 15
+
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
+
+
+age = 16
+
+if age >= 18:
+    print("You can apply for a driving license.")
+else:
+    print("You are not eligible yet.")
