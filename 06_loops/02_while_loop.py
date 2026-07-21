80c2d9197151b1707109a8cf91d994cb23cefe5c
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-21 06:07:32 +0530

    Add while loop demonstration script
    This script demonstrates the use of while loops for counting and countdown.

---
+++ 06_loops/02_while_loop.py
@@ -0,0 +1,21 @@
+"""
+The while Loop
+
+This script demonstrates the while loop.
+"""
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+print("\nCountdown")
+
+number = 5
+
+while number > 0:
+    print(number)
+    number -= 1
+
+print("Blast Off! 🚀")
