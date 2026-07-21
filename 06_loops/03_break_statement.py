3e87e56675a1c3d4d0af7b87c8801931bf6bfa44
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-21 06:08:09 +0530

    Add script to demonstrate break statement in loops
    This script demonstrates the break statement in a loop.

---
+++ 06_loops/03_break_statement.py
@@ -0,0 +1,14 @@
+"""
+The break Statement
+
+This script demonstrates the break statement.
+"""
+
+for number in range(1, 11):
+    if number == 6:
+        print("Breaking the loop...")
+        break
+
+    print(number)
+
+print("Loop finished.")
