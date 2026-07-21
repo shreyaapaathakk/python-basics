1f30543c30ba69ff95165b98e7fb429db34d831d
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-21 06:20:41 +0530

    Add script to demonstrate accessing list items
    This script demonstrates how to access elements in a list using indexing.

---
+++ 08_lists/02_accessing_items.py
@@ -0,0 +1,12 @@
+"""
+Accessing List Items
+
+This script demonstrates how to access elements in a list.
+"""
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+
+print("First color:", colors[0])
+print("Second color:", colors[1])
+print("Last color:", colors[-1])
+print("Second last color:", colors[-2])
