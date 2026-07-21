597306d936a0e11560e04ee982b2bb8a68ae3683
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-21 06:09:22 +0530

    Add script to demonstrate range() function usage
    This script demonstrates different ways to use the range() function in Python.

---
+++ 06_loops/05_range_function.py
@@ -0,0 +1,20 @@
+"""
+The range() Function
+
+This script demonstrates different ways to use range().
+"""
+
+print("range(5)")
+
+for number in range(5):
+    print(number)
+
+print("\nrange(1, 6)")
+
+for number in range(1, 6):
+    print(number)
+
+print("\nrange(2, 11, 2)")
+
+for number in range(2, 11, 2):
+    print(number)
