c2864d805973302ba579f7c380d59664199016c5
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-21 06:22:32 +0530

    Add list slicing demonstration script
    This script demonstrates various list slicing techniques.

---
+++ 08_lists/05_list_slicing.py
@@ -0,0 +1,19 @@
+"""
+List Slicing
+
+This script demonstrates list slicing.
+"""
+
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+print("Original:", numbers)
+
+print("First three:", numbers[:3])
+
+print("Last three:", numbers[-3:])
+
+print("Middle items:", numbers[2:5])
+
+print("Every second item:", numbers[::2])
+
+print("Reverse using slicing:", numbers[::-1])
