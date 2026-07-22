424b3f43b10b38d7fe92fbd713a2beb995eb1ba6
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-22 13:45:40 +0530

    Add script for looping through sets and membership check
    This script demonstrates how to iterate through a set and check membership.

---
+++ 10_sets/05_looping_through_sets.py
@@ -0,0 +1,17 @@
+"""
+Looping Through Sets
+
+This script demonstrates how to iterate through a set.
+"""
+
+colors = {"Red", "Green", "Blue", "Yellow"}
+
+print("Colors:")
+
+for color in colors:
+    print(color)
+
+print("\nChecking Membership")
+
+print("Green" in colors)
+print("Purple" in colors)
