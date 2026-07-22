4e29b36cb2a1774283a84a3d6040fd6459989ad7
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-22 13:33:29 +0530

    Add script for accessing tuple items
    This script demonstrates how to access elements in a tuple, including indexing and slicing.

---
+++ 09_tuples/02_accessing_items.py
@@ -0,0 +1,15 @@
+"""
+Accessing Tuple Items
+
+This script demonstrates how to access elements in a tuple.
+"""
+
+colors = ("Red", "Green", "Blue", "Yellow")
+
+print("First color:", colors[0])
+print("Second color:", colors[1])
+print("Last color:", colors[-1])
+print("Second last color:", colors[-2])
+
+# Slicing
+print("First three colors:", colors[:3])
