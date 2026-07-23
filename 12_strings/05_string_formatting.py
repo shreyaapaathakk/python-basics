e6a6557539e71d20e951a33cbb5a05f1478e6163
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-23 14:42:53 +0530

    Add string formatting demonstration script
    This script demonstrates different ways to format strings using f-strings and the format() method.

---
+++ 12_strings/05_string_formatting.py
@@ -0,0 +1,17 @@
+"""
+String Formatting
+
+This script demonstrates different ways to format strings.
+"""
+
+name = "Alice"
+age = 22
+
+# f-string
+print(f"My name is {name}. I am {age} years old.")
+
+# format()
+print("My name is {}.".format(name))
+
+# Multiple placeholders
+print("{} is {} years old.".format(name, age))
