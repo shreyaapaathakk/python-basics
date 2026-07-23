817cf815789473878b26e8af32d6821cb18faa85
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-23 14:38:05 +0530

    Add string indexing demonstration script
    This script demonstrates accessing characters in a string using indexing.

---
+++ 11_dictionaries/12_strings/02_string_indexing.py
@@ -0,0 +1,12 @@
+"""
+String Indexing
+
+This script demonstrates how to access characters in a string.
+"""
+
+language = "Python"
+
+print("First character:", language[0])
+print("Second character:", language[1])
+print("Last character:", language[-1])
+print("Second last character:", language[-2])
