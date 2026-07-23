c942d08fb9c8b1a3d06e8350ba4276fcd8b3b5de
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-23 14:43:33 +0530

    Add script to demonstrate escape characters
    This script demonstrates common escape characters in Python.

---
+++ 12_strings/06_escape_characters.py
@@ -0,0 +1,13 @@
+"""
+Escape Characters
+
+This script demonstrates common escape characters.
+"""
+
+print("Hello\nWorld")
+
+print("Python\tProgramming")
+
+print("She said, \"Hello!\"")
+
+print("This is a backslash: \\")
