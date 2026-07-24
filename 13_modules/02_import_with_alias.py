f3f6833811db1db56ed4044b98b4e2ae40fd4f76
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-24 12:13:56 +0530

    Add script for importing modules with an alias
    This script demonstrates using aliases while importing modules.

---
+++ 13_modules/02_import_with_alias.py
@@ -0,0 +1,11 @@
+"""
+Importing Modules with an Alias
+
+This script demonstrates using aliases while importing modules.
+"""
+
+import math as m
+
+print("Square Root:", m.sqrt(64))
+print("Ceiling:", m.ceil(5.2))
+print("Floor:", m.floor(5.8))
