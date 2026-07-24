e882f8f385ddb3ffe80b21b52b5fe4a3778bf4ce
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-24 12:15:10 +0530

    Create script to use custom calculator module
    This script demonstrates the use of a custom calculator module by performing basic arithmetic operations.

---
+++ 13_modules/04_creating_custom_module.py
@@ -0,0 +1,12 @@
+"""
+Creating Custom Modules
+
+This script imports functions from a custom module.
+"""
+
+import calculator
+
+print("Addition:", calculator.add(10, 5))
+print("Subtraction:", calculator.subtract(10, 5))
+print("Multiplication:", calculator.multiply(10, 5))
+print("Division:", calculator.divide(10, 5))
