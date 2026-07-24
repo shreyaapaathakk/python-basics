b3106dda7dd2626bb50522cbd389b2d7e77d4fe7
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-24 12:15:35 +0530

    Add script to use functions from custom calculator module
    This script demonstrates how to use specific functions from a custom module for addition and multiplication.

---
+++ 13_modules/05_using_custom_module.py
@@ -0,0 +1,10 @@
+"""
+Using Specific Functions from a Custom Module
+
+This script imports selected functions from a custom module.
+"""
+
+from calculator import add, multiply
+
+print("Addition:", add(15, 20))
+print("Multiplication:", multiply(8, 6))
