0e5454f6b4ec334e87b20d38e4dcd569220ab99e
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-24 12:13:23 +0530

    Add script for importing and using built-in modules
    This script demonstrates importing and using built-in modules like math.

---
+++ 13_modules/01_importing_modules.py
@@ -0,0 +1,11 @@
+"""
+Importing Modules
+
+This script demonstrates how to import and use built-in modules.
+"""
+
+import math
+
+print("Square Root of 25:", math.sqrt(25))
+print("Value of Pi:", math.pi)
+print("Power:", math.pow(2, 5))
