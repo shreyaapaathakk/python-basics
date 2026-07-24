a87e6b9f9c94173d0143802a78f32846dcea3f29
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-24 12:14:35 +0530

    Add script for importing specific items from a module
    This script demonstrates how to import specific functions from the random module and use them to generate a random number and select a random fruit.

---
+++ 13_modules/03_import_specific_items.py
@@ -0,0 +1,13 @@
+"""
+Importing Specific Items
+
+This script demonstrates importing specific functions from a module.
+"""
+
+from random import randint, choice
+
+print("Random Number:", randint(1, 10))
+
+fruits = ["Apple", "Banana", "Orange", "Mango"]
+
+print("Random Fruit:", choice(fruits))
