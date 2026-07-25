14b1065400f4715fa0223415398e4461b294dbba
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-25 10:44:15 +0530

    Create Student class and instantiate objects
    This script introduces a simple Student class and demonstrates object instantiation.

---
+++ 16_oop/01_classes_objects.py
@@ -0,0 +1,19 @@
+"""
+Classes and Objects
+
+This script demonstrates how to create a class
+and instantiate objects.
+"""
+
+
+class Student:
+    """A simple Student class."""
+
+    pass
+
+
+student1 = Student()
+student2 = Student()
+
+print(type(student1))
+print(type(student2))
