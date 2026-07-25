9651441799966f25af4fc7ed45d0318d484b3d2a
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-25 10:47:04 +0530

    Add encapsulation example with BankAccount class
    This script demonstrates encapsulation in a BankAccount class, including methods for depositing and retrieving the balance.

---
+++ 16_oop/07_encapsulation.py
@@ -0,0 +1,25 @@
+"""
+Encapsulation
+
+This script demonstrates encapsulation.
+"""
+
+
+class BankAccount:
+    """Represents a bank account."""
+
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+
+account = BankAccount(1000)
+
+account.deposit(500)
+
+print("Balance:", account.get_balance())
