356d9bfec710ec3956f317909b6134c4a2a38b51
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-26 10:52:19 +0530

    Implement operator overloading for Point class
    This script demonstrates operator overloading with a Point class that supports addition.

---
+++ 17_advanced_oop/08_operator_overloading.py
@@ -0,0 +1,28 @@
+"""
+Operator Overloading
+
+This script demonstrates operator overloading.
+"""
+
+
+class Point:
+    """Represents a point."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(
+            self.x + other.x,
+            self.y + other.y
+        )
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+point1 = Point(2, 3)
+point2 = Point(4, 5)
+
+print(point1 + point2)
