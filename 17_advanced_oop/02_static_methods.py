a26befb0ce607c4c204c08d122f0ff36f3e7a0bf
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-26 10:49:41 +0530

    Add static method for addition in Calculator class
    Demonstrate the use of static methods in a Calculator class.

---
+++ 17_advanced_oop/02_static_methods.py
@@ -0,0 +1,16 @@
+"""
+Static Methods
+
+This script demonstrates static methods.
+"""
+
+
+class Calculator:
+    """Simple calculator."""
+
+    @staticmethod
+    def add(number1, number2):
+        return number1 + number2
+
+
+print(Calculator.add(10, 5))
