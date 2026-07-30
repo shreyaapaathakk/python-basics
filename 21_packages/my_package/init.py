1de75770dc1ef049cd0134eac4e21158e45c8793
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-30 16:02:39 +0530

    Create init.py for my_package with imports

---
+++ 21_packages/my_package/init.py
@@ -0,0 +1,9 @@
+"""
+my_package
+
+Example Python package.
+"""
+
+from .calculator import add, subtract, multiply, divide
+from .greetings import say_hello, say_goodbye
+from .utilities import roll_dice, is_even
