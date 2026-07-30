073d821d73ed6c1ba0f61d3affeb7d8c2fd1228d
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-30 16:01:50 +0530

    Add greeting functions for hello and goodbye

---
+++ 21_packages/my_package/greetings.py
@@ -0,0 +1,15 @@
+"""
+greetings.py
+
+Greeting functions.
+"""
+
+
+def say_hello(name):
+    """Return a greeting message."""
+    return f"Hello, {name}!"
+
+
+def say_goodbye(name):
+    """Return a goodbye message."""
+    return f"Goodbye, {name}!"
