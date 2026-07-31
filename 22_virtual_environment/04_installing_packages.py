d25695ed996c416002d4c7c50c05effaea376ea8
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-31 12:50:24 +0530

    Create 04_installing_packages.py with pip commands
    Added instructions for installing, upgrading, and uninstalling packages using pip.

---
+++ 22_virtual_environment/04_installing_packages.py
@@ -0,0 +1,20 @@
+"""
+04_installing_packages.py
+
+Installing packages using pip.
+"""
+
+print("Install a package")
+print("pip install requests")
+
+print("\nInstall a specific version")
+print("pip install requests==2.32.0")
+
+print("\nUpgrade a package")
+print("pip install --upgrade requests")
+
+print("\nUninstall a package")
+print("pip uninstall requests")
+
+print("\nList installed packages")
+print("pip list")
