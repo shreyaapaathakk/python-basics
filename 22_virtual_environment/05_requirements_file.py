28d168bd85b901184eb4a6174bb85e0d454b1e5c
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-31 12:50:50 +0530

    Create 05_requirements_file.py for requirements management
    Add a script to manage requirements.txt for dependencies.

---
+++ 22_virtual_environment/05_requirements_file.py
@@ -0,0 +1,18 @@
+"""
+05_requirements_file.py
+
+Working with requirements.txt.
+"""
+
+print("Create requirements.txt")
+print("pip freeze > requirements.txt")
+
+print("\nInstall all dependencies")
+print("pip install -r requirements.txt")
+
+print("\nExample requirements.txt")
+print("""
+requests==2.32.0
+numpy==2.3.1
+pandas==2.3.0
+""")
