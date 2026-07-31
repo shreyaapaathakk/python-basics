fcaa1a55c0fa37525d4eed94730f966463d6f3b4
Shreya Pathak <shreyaapaathakk@gmail.com>
2026-07-31 12:49:21 +0530

    Add script for creating a virtual environment
    This script provides instructions for creating a virtual environment on Windows and macOS/Linux.

---
+++ 22_virtual_environment/02_creating_virtual_environment.py
@@ -0,0 +1,15 @@
+"""
+02_creating_virtual_environment.py
+
+Creating a virtual environment.
+"""
+
+print("Create a virtual environment:")
+
+print("\nWindows")
+print("python -m venv venv")
+
+print("\nmacOS / Linux")
+print("python3 -m venv venv")
+
+print("\nA folder named 'venv' will be created.")
